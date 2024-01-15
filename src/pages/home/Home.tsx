6b9f082c66d77683221e32a4df09dc62d1298b59
Vinicius Lima <vinicius.slima99@icloud.com>
2024-01-15 16:20:04 -0300

    Criando o projeto react

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo' >Home</h1>
+            <img src="https://i.imgur.com/UupJoOo.jpg" alt="Imagem inicial" className='img' />
+           
+        </>
+    );
+}
+
+export default Home;
