289bfe6f9c1d4e99bae3b38efab71d3554d712d9
Vinicius Lima <vinicius.slima99@icloud.com>
2024-01-22 11:04:57 -0300

    Criando o CRUD da model Tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -5,6 +7,9 @@ import Login from './pages/login/Login';
 import Cadastro from './pages/register/Cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/themes/listaTemas/ListaTemas';
+import FormularioTema from './components/themes/formularioTema/FormularioTema';
+import DeletarTema from './components/themes/deletarTema/DeletarTema';
 
 
 function App() {
@@ -19,6 +24,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,29 +1,41 @@
+import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 
 function Navbar() {
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
+  }
+
+  let navbarComponent
 
 
+  return (
+    <>
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-    return (
-        <>
-            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-                <div className="container flex justify-between text-lg">
-                    <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+          <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+            <div className='hover:underline'>Perfil</div>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
 
-                    <div className='flex gap-4'>
-
-                        <div className='hover:underline'>Postagens</div>
-                        <div className='hover:underline'>Temas</div>
-                        <div className='hover:underline'>Cadastrar tema</div>
-                        <div className='hover:underline'>Perfil</div>
-                        <div className='hover:underline'>Sair</div>
-
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Navbar
